8c94362a4f733a36df51cead74171b8b8758970a
Event 42 <event204@e2r9p19.42.fr>
2016-01-17 02:41:05 +0100

    update de l'index avec l'ajout de socket.io et debut de l'update des controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,45 +1,92 @@
 angular.module('starter.controllers', [])
 //////////////////////////////////////////////////////
 .controller('MapCtrl', function($scope, $ionicLoading) {
-  $scope.mapCreated = function(map) {
-    $scope.map = map;
-  };
+	$scope.mapCreated = function(map) {
+		$scope.map = map;
+	};
+	$scope.addSafeMarker = function() {
+		$scope.statut = Safe.getStatut();
+		console.log("ajout du safe marqueur en cours");
 
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
-    }
+		navigator.geolocation.getCurrentPosition(function (pos) {
+			console.log('position?', pos);
+			$scope.latitude = pos.coords.latitude;
+			$scope.longitude = pos.coords.longitude;
+			console.log($scope.latitude);
+			console.log($scope.longitude);
+			$scope.coord = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+			$scope.marker = new google.maps.Marker({
+				icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+				position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+				/*animation: google.maps.Animation.DROP,*/
+				map: $scope.map
+			});
+		}, function(error) {
+			alert('Unable to get location: ' + error.message);
+		})
+		socket.emit('flag', {
+			lat: $scope.latitude.toString(),
+			lng: $scope.longitude.toString(),
+			status: "safe"
+		})
+	}
+	$scope.addUnSafeMarker = function() {
+		$scope.statut = Safe.getStatut();
+		console.log("ajout du safe marqueur en cours");
 
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: true
-    });
+		navigator.geolocation.getCurrentPosition(function (pos) {
+			console.log('position?', pos);
+			$scope.coord = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+			$scope.marker = new google.maps.Marker({
+				icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+				position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+				/*animation: google.maps.Animation.DROP,*/
+				map: $scope.map
+			});
+		}, function (error) {
+			alert('Unable to get location: ' + error.message);
+		})
+		socket.emit('flag', {
+			lat: $scope.latitude.toString(),
+			lng: $scope.longitude.toString(),
+			status: "unsafe"
+		})
+	}
+	$scope.centerOnMe = function () {
+		console.log("Centering");
+		if (!$scope.map) {
+			return;
+		}
 
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
+		$scope.loading = $ionicLoading.show({
+			content: 'Getting current location...',
+			showBackdrop: true
+		});
+
+		navigator.geolocation.getCurrentPosition(function (pos) {
+			console.log('Got pos', pos);
+			$scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+			$scope.loading.hide();
+		}, function (error) {
+			alert('Unable to get location: ' + error.message);
+		});
+	};
 })
 /////////////////////////////////////////////////////////
 .controller('securiteCtrl', function($scope, $ionicLoading) {
-    $scope.loading = $ionicLoading.show({
-        content: 'Veuillez patienter nous essayons d\'envoyer les informations sur le serveur',
-        showBackdrop: true
-    });
+	$scope.loading = $ionicLoading.show({
+		content: 'Veuillez patienter nous essayons d\'envoyer les informations sur le serveur',
+	showBackdrop: true
+	});
 })
 ////////////////////////////////////////////////////////
 .controller('AppController', function($scope) {
-    $scope.leftButtons = [{
-        type: 'button-icon icon ion-navicon',
-        tap: function(e) {
-            $scope.sideMenuController.toggleLeft();
-        }
-    }];
+	$scope.leftButtons = [{
+		type: 'button-icon icon ion-navicon',
+tap: function(e) {
+	$scope.sideMenuController.toggleLeft();
+}
+}];
 })
 /////////////////////////////////////////////////////////
 .controller('Page1Controller', function($scope) {
@@ -47,10 +94,10 @@ angular.module('starter.controllers', [])
 })
 
 .controller('Page2Controller', function($scope) {
-    $scope.leftButtons = [{
-        type: 'button-icon icon ion-navicon',
-        tap: function(e) {
-            $scope.sideMenuController.toggleLeft();
-        }
-    }];
+	$scope.leftButtons = [{
+		type: 'button-icon icon ion-navicon',
+tap: function(e) {
+	$scope.sideMenuController.toggleLeft();
+}
+}];
 });
